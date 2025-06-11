549ac3dbd60b8286777e7712de73550727d8baec
Javier Alvarado <77702824+XaviAlvarado18@users.noreply.github.com>
2025-06-10 23:13:23 -0600

    chore: home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,9 +7,14 @@ export const HomePage: React.FC = () => {
   const navigate = useNavigate()
 
   return (
-    <div className="flex items-center justify-center h-screen w-screen bg-amber-900">
-      <Card title="Bienvenido" className="w-72">
-        <p>¡Ya estás logueado!</p>
+    <div className="flex flex-col items-center justify-center h-screen w-screen bg-white">
+      <div className="w-full h-16 bg-[#92D050]" >
+        <p>Pene xD</p>
+      </div>
+
+      <div className='flex-1 flex items-center justify-center bg-amber-950'>
+      <Card title="Bienvenido" className="w-72 bg-amber-950">
+        <p className='Open-Sans'>¡Ya estás logueado!</p>
         <Button
           type="primary"
           block
@@ -19,6 +24,7 @@ export const HomePage: React.FC = () => {
           Cerrar sesión
         </Button>
       </Card>
+      </div>
     </div>
   )
 }
