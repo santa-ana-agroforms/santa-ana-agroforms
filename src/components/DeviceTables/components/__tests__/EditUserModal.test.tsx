a297cf25af933b922d95b60a7ae43796be4fce0f
Mario Guerra <maguerramorales@gmail.com>
2025-09-16 20:14:34 -0600

    fix: react imports in tests

--- src/components/DeviceTables/components/__tests__/EditUserModal.test.tsx
+++ src/components/DeviceTables/components/__tests__/EditUserModal.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import EditUserModal, { EditUserValues } from '@/components/DeviceTables/components/EditUserModal';
 
--- src/components/FlatTables/__tests__/FlatTables.test.tsx
+++ src/components/FlatTables/__tests__/FlatTables.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import FlatTables, { type Category } from '@/components/FlatTables';
 
