490327139d7f07cd76814e25afaf8787412626a2
Mario Guerra <maguerramorales@gmail.com>
2025-10-20 02:12:06 -0600

    fix: Cypress and Selenium workflows for some reason

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import istanbul from "vite-plugin-istanbul";
 
 const __filename = fileURLToPath(import.meta.url);
 
@@ -44,13 +43,8 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    cspHeaderPlugin(), istanbul({
-      cypress: true,
-      requireEnv: false,
-      include: ["src/**/*"],
-      extension: [".ts", ".tsx", ".js", ".jsx"],
-      exclude: ["node_modules", "cypress", "mocha_tests", "selenium_tests"]
-    })],
+    cspHeaderPlugin()
+  ],
   resolve: {
     alias: {
       "@": new URL("./src", import.meta.url).pathname,
